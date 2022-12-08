e41cc73e42aca2ace58aa77567e32ba51f1fba5e
waveringdonut <k20rs602@st.kyusan-u.ac.jp>
2022-12-08 17:38:34 +0900

    ニフクラモバイルバックエンドへデータを保存する機能を追加

--- www/js/tapGame.js
+++ www/js/tapGame.js
@@ -36,19 +36,24 @@ function startGame() {
 
 // 【mBaaS】データの保存
 function saveScore (name, score) {
-    // **********【問題１】名前とスコアを保存しよう！**********
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // ********************************************************
+    // 保存先クラスを作成
+    var GameScore = ncmb.DataStore("GameScore");
+    // クラスインスタンスを生成
+    var gameScore = new GameScore();
+    // 値を設定
+    gameScore.set("name", name);
+    gameScore.set("score", score);
+    // 保存を実施
+    gameScore.save()
+            .then(function (){
+                // 保存に成功した場合の処理
+                console.log("保存に成功しました。");
+            })
+            .catch(function (error){
+                // 保存に失敗した場合の処理
+                console.log("保存に失敗しました。エラー:" + error); 
+            });
+// **************************************************
 }
 
 // タイマー
